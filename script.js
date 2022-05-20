30ac21625bb06e4598e334ad07b1c9123592ef4e
jnsltk <janos.litkei@gmail.com>
2022-05-20 16:35:21 +0800

    Make buttons input numbers to the display

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const display = document.querySelector('#display');
+const btnNum1 = document.querySelector('#button-1');
+const btnNum2 = document.querySelector('#button-2');
+const btnNum3 = document.querySelector('#button-3');
+const btnNum4 = document.querySelector('#button-4');
+const btnNum5 = document.querySelector('#button-5');
+const btnNum6 = document.querySelector('#button-6');
+const btnNum7 = document.querySelector('#button-7');
+const btnNum8 = document.querySelector('#button-8');
+const btnNum9 = document.querySelector('#button-9');
+const btnNum0 = document.querySelector('#button-0');
+
+let displayValue = '';
+
+// Basic calculator functions
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -12,12 +27,13 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   if (!num2) {
-    alert("Ye can't divide by zero ye eejit");
-    return "nah";
+    alert('Ye can\'t divide by zero ye eejit');
+    return 'nah';
   }
   return num1 / num2;
 }
 
+// Operator function
 function operate(operator, num1, num2) {
   switch (operator) {
     case add:
@@ -29,6 +45,25 @@ function operate(operator, num1, num2) {
     case divide:
       return divide(num1, num2);
     default:
-      return "error";
+      return 'error';
   }
 }
+
+/* This function gets called when the user presses a button,
+checks which button was pressed, and adds that number to displayValue */
+function btnPress(e) {
+  displayValue += `${e.target.id.slice(-1)}`;
+  // console.log(e.target.id.slice(-1));
+  display.value = displayValue;
+}
+
+btnNum1.addEventListener('click', btnPress);
+btnNum2.addEventListener('click', btnPress);
+btnNum3.addEventListener('click', btnPress);
+btnNum4.addEventListener('click', btnPress);
+btnNum5.addEventListener('click', btnPress);
+btnNum6.addEventListener('click', btnPress);
+btnNum7.addEventListener('click', btnPress);
+btnNum8.addEventListener('click', btnPress);
+btnNum9.addEventListener('click', btnPress);
+btnNum0.addEventListener('click', btnPress);
