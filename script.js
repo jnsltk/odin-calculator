33f9f57be4cd5027a4554017fa7cad9379103d94
jnsltk <janos.litkei@gmail.com>
2022-05-20 14:29:17 +0800

    Add ESLint to the project

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function add(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
-    if (!num2) {
-        alert('Ye can\'t divide by zero ye eejit'); 
-        return 'nah mate';
-    }
-    return num1 / num2;
+  if (!num2) {
+    alert('Ye can\'t divide by zero ye eejit');
+    return 'nah';
+  }
+  return num1 / num2;
 }
