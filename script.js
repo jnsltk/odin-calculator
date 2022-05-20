ecd51018df32196ec88824dab039ba983ff462f6
jnsltk <janos.litkei@gmail.com>
2022-05-20 14:38:09 +0800

    Add operate function

--- script.js
+++ script.js
@@ -12,8 +12,23 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   if (!num2) {
-    alert('Ye can\'t divide by zero ye eejit');
-    return 'nah';
+    alert("Ye can't divide by zero ye eejit");
+    return "nah";
   }
   return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case add:
+      return add(num1, num2);
+    case subtract:
+      return subtract(num1, num2);
+    case multiply:
+      return multiply(num1, num2);
+    case divide:
+      return divide(num1, num2);
+    default:
+      return "error";
+  }
 }
